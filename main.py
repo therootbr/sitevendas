1b3a6db119ba998180239a935122e21baf99afef
TheRooT <83381422+therootbr@users.noreply.github.com>
2022-07-21 00:15:33 +0000

    Adicionado DB com Alchemy

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
 import json
+from flask import flash, redirect
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+conexao = "sqlite:///meubanco.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -18,5 +32,17 @@ def clientes():
   arquivo = open("static/clientes.json")
   clientes = json.load(arquivo)
   return render_template('clientes.html', clientes=clientes)
+
+@app.route('/teste_insert')
+def teste_insert():
+  u = Usuario("Alba Lopes", "emaildealba@hotmail.com", "1234")
+  db.session.add(u)
+  db.session.commit()
+  return 'Dados insediros com sucesso'
+
+@app.route('/teste_select')
+def teste_select():
+  u = Usuario.query.get(1)
+  return u.nome
 
 app.run(host='0.0.0.0', port=81)
